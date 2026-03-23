2337486f9f94644c95d48720fe79b5d2df5d4e20
Fenish <patelfenish245@gmail.com>
2026-03-23 00:15:51 -0400

    Add files via upload
    Add & setup the main files to work on !!

---
+++ COIL_18/COIL_Milestone1/COIL_Milestone1_Tests/COIL_Milestone1_Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace COILMilestone1Tests
+{
+	TEST_CLASS(COILMilestone1Tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
